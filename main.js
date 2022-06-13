493f2aa0fefb932e42257866d0ae35e36fb4e7c2
mdsabirali786 <md26sabir.ali@gmail.com>
2022-06-14 03:44:26 +0530

    Update
    Update

---
+++ main.js
@@ -0,0 +1,11 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+
+
+
+
